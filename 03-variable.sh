3777f4d50ebb2b36bae87dfd57bf4bfb24fdd455
Amar <cberg2386@gmail.com>
2024-09-08 11:55:39 +0530

    variables

---
+++ 03-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#creating convo between Amar and pavan and will add them as variable
+
+NAME1=Amar
+NAME2=Pavan
+
+echo " $NAME1: Hello $NAME2, How are you?"
+echo " $NAME2: Hello $NAME1, Hope you're doing fine."
+echo " $NAME1: yes, I'm doing very well."
+echo " $NAME2: $NAME1, Are you doing job?"
