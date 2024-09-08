c6c035135db79a7e8456e7ae8589a89aa4489804
Amar <cberg2386@gmail.com>
2024-09-08 12:06:01 +0530

    variables

---
+++ 04-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#passing arguments to the file
+
+NAME1=$arg1
+NAME2=$arg2
+
+echo " $NAME1: Hello ${NAME2}, How are you?"
+echo " $NAME2: Hello $NAME1, Hope you're doing fine."
+echo " ${NAME1}: yes, I'm doing very well."
+echo " $NAME2: $NAME1, Are you doing job?"
