275feeddfb8edadade8c8e13f4d4c5944cca6f7a
Amar <cberg2386@gmail.com>
2024-09-08 11:57:55 +0530

    variables

--- 03-variable.sh
+++ 03-variable.sh
@@ -5,7 +5,7 @@
 NAME1=Amar
 NAME2=Pavan
 
-echo " $NAME1: Hello $NAME2, How are you?"
+echo " $NAME1: Hello {$NAME2}, How are you?"
 echo " $NAME2: Hello $NAME1, Hope you're doing fine."
-echo " $NAME1: yes, I'm doing very well."
+echo " {$NAME1}: yes, I'm doing very well."
 echo " $NAME2: $NAME1, Are you doing job?"
