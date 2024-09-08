d016d061b7f21f51c2bf9197103f6e4b53bf78ad
Amar <cberg2386@gmail.com>
2024-09-08 13:03:29 +0530

    array modified

--- 06-array.sh
+++ 06-array.sh
@@ -3,4 +3,4 @@
 fruits=("mango" "banana" "grapes" "sapota")
 
 echo " first fruit of the list is: ${fruit[0]}"
-echoo " last fruit of the list is: ${fruit[n]}"
+echoo " last fruit of the list is: ${fruit[4]}"
