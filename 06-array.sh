ce55e0e69de8f2af2547c59c7b7e1a98e20eb09d
Amar <cberg2386@gmail.com>
2024-09-08 13:11:13 +0530

    variable

--- 06-array.sh
+++ 06-array.sh
@@ -3,4 +3,4 @@
 fruits=("mango" "banana" "grapes" "sapota")
 
 echo " first fruit of the list is: ${fruits[0]}"
-echo " last fruit of the list is: ${fruits[4]}"
+echo " last fruit of the list is: ${fruits[-1]}"
