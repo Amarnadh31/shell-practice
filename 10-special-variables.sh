3e636b33dc268197c2d2eedfd59364e628bf8644
Amar <cberg2386@gmail.com>
2024-09-11 06:42:12 +0530

    special variables

--- 10-special-variables.sh
+++ 10-special-variables.sh
@@ -2,3 +2,9 @@
 
 echo "All the variable indiacated with $@"
 echo " number of variables passed now : $#"
+echo " script name : $0"
+echo " current working directory $PWD"
+echo " home directory of current user $HOME"
+echo " pID of the current script : $$"
+sleep 100 &
+echo "PID of the last background process : $!"
