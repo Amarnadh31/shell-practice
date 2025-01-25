300dbfc249a5c75b6f9fbb917a3b2c062b22abe6
Amar <cberg2386@gmail.com>
2025-01-25 21:54:32 +0530

    shell script

--- 15-array.sh
+++ 15-array.sh
@@ -3,6 +3,6 @@
 heros=("aa" "mahesh" "tarak")
 
 echo "first hero name ${heros[0]}"
-echo "second hero name ${hero[2]}"
+echo "second hero name ${heros[2]}"
 
-echo "last hero name ${hero[@]}"
+echo "last hero name ${heros[@]}"
