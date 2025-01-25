2e0c00b4211424e76096f29dd5ed34273f6b0c9d
Amar <cberg2386@gmail.com>
2025-01-25 23:53:14 +0530

    shell script

--- 16-builtin.sh
+++ 16-builtin.sh
@@ -8,4 +8,6 @@ sum=$(($num1+$num2))
 echo "sum of $num1 and $num2 is ${sum}"
 
 echo "total variables $#"
-
+echo "aruguments passed to the script $@"
+echo "present process $!"
+echo " previous process $*"
