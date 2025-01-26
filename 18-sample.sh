3e0007bb068c8fd1b724c0f481e464858d983521
Amar <cberg2386@gmail.com>
2025-01-26 17:24:55 +0530

    shell script

---
+++ 18-sample.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]
+then
+    echo "kindly run with root user"
+    exit 1
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "my sql not installed...Installing"
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then 
+        echo "mysql installation failed.."
+        exit 1
+    else
+        echo "mysql installed successfully.."
+else
+    echo "mysql is already installed, nothing to do
+
+
+
+
+
